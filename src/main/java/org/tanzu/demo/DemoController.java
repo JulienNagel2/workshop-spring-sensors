c44cbc593c96660ac8aae3ad3c285f1f2a244e92
JulienNagel2 <julien51nagel@gmail.com>
2024-07-03 15:41:01 +0200

    Added Github Workflow

--- src/main/java/org/tanzu/demo/DemoController.java
+++ src/main/java/org/tanzu/demo/DemoController.java
@@ -52,7 +52,7 @@ public class DemoController {
         return new HashMap<>();
     }
 
-    private static final String REPLACEMENT_BANNER_TEXT="Spring Sensors JNG1.2";
+    private static final String REPLACEMENT_BANNER_TEXT="Spring Sensors JNG1.3";
 
     @RequestMapping("/refresh")
     public @ResponseBody SensorData refresh() {
